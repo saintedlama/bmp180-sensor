4e835dbab7236680b27b1947c01caf3e509fc6c2
saintedlama <christoph.walcher@gmail.com>
2026-01-29 20:18:25 +0100

    reformat code

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Sensor = require('./lib/sensor')
+const Sensor = require("./lib/sensor");
 
-module.exports = async function(options) {
-  options = options || {}
-  options.bus = options.bus || 1
-  options.mode = options.mode || 1
-  options.address = options.address || 0x77
+module.exports = async function (options) {
+  options = options || {};
+  options.bus = options.bus || 1;
+  options.mode = options.mode || 1;
+  options.address = options.address || 0x77;
 
   // Allow dependency injection for testing, lazy-load i2c-bus only when needed
-  let i2cOpen
+  let i2cOpen;
   if (options.i2cBus) {
-    i2cOpen = options.i2cBus
+    i2cOpen = options.i2cBus;
   } else {
-    const i2c = require('i2c-bus')
-    i2cOpen = i2c.openSync(options.bus)
+    const i2c = require("i2c-bus");
+    i2cOpen = i2c.openSync(options.bus);
   }
 
-  const sensor = new Sensor(i2cOpen, options)
-  await sensor.calibrate()
+  const sensor = new Sensor(i2cOpen, options);
+  await sensor.calibrate();
 
-  return sensor
+  return sensor;
 };
